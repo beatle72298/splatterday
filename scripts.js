837e882461c8a89e49e4cd4a30ea2b4161e74ce8
beatle72298 <47221891+beatle72298@users.noreply.github.com>
2022-11-09 16:49:49 -0600

    JS button interaction update (desktop/mobile media query)

--- scripts.js
+++ scripts.js
@@ -1,17 +1,29 @@
 
 $(document).ready(function(){    
-let link = $("#comp-l6w88giv > p > span > a").attr("href");
-console.log(link);
+//let link = $("#comp-l6w88giv > p > span > a").attr("href");
+//console.log(link);
+//
+//setTimeout(function(){
+//    let chessBtn = $("._3Xz9Z").contents().find("#chess-btn");   
+//    let onClick = "popup({url:'"+link+"',title: 'Chess',w:1025,h:560,left:690}"
+//
+//    chessBtn.attr("onclick",onClick);
+//    console.log(chessBtn); 
+//}, 2000);
 
-setTimeout(function(){
-    let chessBtn = $("._3Xz9Z").contents().find("#chess-btn");   
-    let onClick = "popup({url:'"+link+"',title: 'Chess',w:1025,h:560,left:690}"
+function changeBtn(x) {
+  if (x.matches) { // If media query matches
+    $('#splatterday-btn').href = "https://theknightschool.com/splatterday";
+    $("#splatterday-btn").attr("onClick","");
+  } else {
+      $('#splatterday-btn').href = "";
+      $("#splatterday-btn").attr("onClick","javascript:window.close('','_parent','');");
+  }
+}
 
-    chessBtn.attr("onclick",onClick);
-    console.log(chessBtn); 
-}, 2000);
-
- 
+var x = window.matchMedia("(max-width: 500px)")
+changeBtn(x) // Call listener function at run time
+x.addListener(changeBtn) // Attach listener function on state changes
 
 //$("._3Xz9Z").on('load', function(){
 //let chessBtn = $("._3Xz9Z").contents().find("#chess-btn");   
