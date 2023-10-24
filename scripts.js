7801183ab004c4921d1d1ab05dbf5c2bb6ecaa10
beatle72298 <47221891+beatle72298@users.noreply.github.com>
2023-10-24 08:39:15 -0500

    updates

--- scripts.js
+++ scripts.js
@@ -1,15 +1,6 @@
 
 $(document).ready(function(){    
-//let link = $("#comp-l6w88giv > p > span > a").attr("href");
-//console.log(link);
-//
-//setTimeout(function(){
-//    let chessBtn = $("._3Xz9Z").contents().find("#chess-btn");   
-//    let onClick = "popup({url:'"+link+"',title: 'Chess',w:1025,h:560,left:690}"
-//
-//    chessBtn.attr("onclick",onClick);
-//    console.log(chessBtn); 
-//}, 2000);
+
 
 function changeBtn(x) {
   if (x.matches) { // If media query matches
@@ -27,11 +18,4 @@ var x = window.matchMedia("(max-width: 500px)")
 changeBtn(x) // Call listener function at run time
 x.addListener(changeBtn) // Attach listener function on state changes
 
-//$("._3Xz9Z").on('load', function(){
-//let chessBtn = $("._3Xz9Z").contents().find("#chess-btn");   
-//let onClick = "popup({url:'"+link+"',title: 'Chess',w:1025,h:560,left:690}"
-//    
-//chessBtn.attr("onclick",onClick);
-//console.log(chessBtn);     
-//});
 });
